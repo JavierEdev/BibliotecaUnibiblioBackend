e49f05b31f5b8367b2de7041f64c3b6b9fca6103
Javier E <estradajuarezjavier01@gmail.com>
2024-04-15 22:15:09 -0600

    Commit 2: Alterations to base tables

---
+++ TABLES/tbl_alterationsBaseTables.sql
@@ -0,0 +1,8 @@
+ALTER TABLE usuarios ADD contrasena VARCHAR(500)
+ALTER TABLE usuarios ADD fechaCrea DATETIME
+
+ALTER TABLE registroMaestro ADD fechaCrea DATETIME
+
+ALTER TABLE registroDetalle ADD fechaCrea DATETIME
+
+ALTER TABLE suspensiones ADD fechaCrea DATETIME
