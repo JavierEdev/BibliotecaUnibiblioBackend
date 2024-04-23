763a566003c9ef16850996e57b6b01cc8213c6c4
JostinLinares03 <jostinlinares2003@gmail.com>
2024-04-22 18:08:20 -0600

    Creacion Sp_selectLibros
    Procedimiento creado para visualizar los libros en el catalogo

---
+++ PROCEDURES/sp_SelectLibros.sql
@@ -0,0 +1,32 @@
+USE UNB_Q01
+GO
+
+IF EXISTS
+ (
+     SELECT TOP 1 1
+     FROM sysobjects
+     WHERE type = 'P'
+     AND name = 'sp_SelectLibros'
+ )
+BEGIN
+   DROP PROCEDURE sp_SelectLibros
+END
+GO
+
+CREATE PROCEDURE SelectLibros
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT 
+        [idLibro],
+        [nombre],
+        [autor],
+        [anioPublicacion],
+        [grupoLibro],
+        [enStock],
+        [estado],
+        [tipoIdentificador],
+        [idArea]
+    FROM 
+        [UNB_Q01].[dbo].[libros];
+END;
