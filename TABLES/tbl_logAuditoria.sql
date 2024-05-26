8fd316884f104e1ba57b66cb2e53327c92ab0cd5
Javier E <estradajuarezjavier01@gmail.com>
2024-05-26 16:45:33 -0600

    Commit 15: Renombrada la tabla dentro del script

---
+++ TABLES/tbl_logAuditoria.sql
@@ -0,0 +1,10 @@
+CREATE TABLE logAuditoriaUsuarios (
+   IdEntrada     INT IDENTITY(1,1) PRIMARY KEY,
+   NombreTabla   NVARCHAR(100),
+   Operacion     NVARCHAR(10),
+   DataVieja     NVARCHAR(MAX),
+   DataNueva     NVARCHAR(MAX),
+   fechaOperacion DATETIME DEFAULT GETDATE(),
+   usuarioModifica NVARCHAR(MAX)
+)
+GO
